d930207ec807cc34d3a09f0ed87816a0048f01d2
Ahmed Salem <ahmed.salem1997@gmail.com>
2025-07-23 00:55:43 +0300

    auth

--- packages/features/auth/src/schemas/password-sign-up.schema.ts
+++ packages/features/auth/src/schemas/password-sign-up.schema.ts
@@ -4,7 +4,6 @@ import { RefinedPasswordSchema, refineRepeatPassword } from './password.schema';
 
 export const PasswordSignUpSchema = z
   .object({
-    fullName: z.string().min(1, {}),
     email: z.string().email(),
     password: RefinedPasswordSchema,
     repeatPassword: RefinedPasswordSchema,
